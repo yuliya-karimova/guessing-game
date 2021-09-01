f0a10d0c95436089e7c9a0bff894f37ffd314d9b
yuliya-karimova <y.o.karimova@gmail.com>
2021-09-01 11:58:47 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.max - this.min) / 2 + this.min);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
